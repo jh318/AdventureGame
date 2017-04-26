b2727c4a4f05f1b9cd1fea851c1036e621bae2be
John Hawley <jhawley@institute.local>
2017-04-25 22:41:54 -0500

    init commit

---
+++ Assets/Character/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 5;
+	public float turnSpeed = 3;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+
+		anim.SetFloat("forwardVelocity", y * speed);
+		anim.SetFloat("turnVelocity", x * turnSpeed);
+		anim.SetFloat("speed", x*x + y*y);
+
+		if (Input.GetButtonDown ("Jump")) {
+			anim.SetTrigger ("playerDeath");
+		}
+		if (Input.GetButtonDown ("Fire2")) {
+			anim.SetTrigger ("fireball");
+
+		}
+		if (Input.GetButtonDown ("Fire3")) {
+			anim.SetTrigger ("hitReactBackwards");
+		}
+	}
+}
