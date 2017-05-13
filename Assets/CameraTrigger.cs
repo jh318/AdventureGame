d66da3eb33dc44f14acfb973b7587527fb6b5b53
John Hawley <jhawley@institute.local>
2017-05-12 19:05:50 -0500

    created camera trigger script and prefab

---
+++ Assets/CameraTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour {
+
+	public Transform cameraTransform;
+
+	void OnTriggerEnter()
+	{
+		Camera.main.transform.position = cameraTransform.position;
+		Camera.main.transform.rotation = cameraTransform.rotation;
+
+	}
+}
