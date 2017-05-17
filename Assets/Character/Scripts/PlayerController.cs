ed245400abc7c4dad2f2698df2b0fba820ac1f54
John Hawley <jhawley@institute.local>
2017-05-16 23:19:26 -0500

    added camera stuff and player movement stuff and back animation

--- Assets/Character/Scripts/PlayerController.cs
+++ Assets/Character/Scripts/PlayerController.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 5;
-	public float turnSpeed = 3;
+	public float maxSpeed = 5;
+	public float maxTurnSpeed = 3.5f;
+	public float maxForwardVelocity = 5f;
+	public float maxTurnVelocity = 3.5f;
 
 	private Animator anim;
 	private HealthController health;
+	private Rigidbody body;
+
 
 	// Use this for initialization
 	void Start () {
 		health = GetComponent<HealthController> ();
 		health.onHealthChanged += AnimateHealth;
 		anim = GetComponent<Animator>();
+		body = GetComponent<Rigidbody> ();
 	}
 
 	void AnimateHealth(float health, float prevHealth, float maxHealth){
@@ -27,13 +32,26 @@ public class PlayerController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float x = Input.GetAxis("Horizontal");
-		float y = Input.GetAxis("Vertical");
+		Transform cam = Camera.main.transform;
+		Vector3 targetRight = Input.GetAxis ("Horizontal") * cam.right;
+		Vector3 targetForward = Input.GetAxis ("Vertical") * cam.forward;
+		Vector3 target = targetRight + targetForward;
+		target.y = 0;
 
-		anim.SetFloat("forwardVelocity", y * speed);
-		anim.SetFloat("turnVelocity", x * turnSpeed);
-		anim.SetFloat("speed", x*x + y*y);
+		body.velocity = (target).normalized * maxSpeed;
 
+		Vector3 heading = body.velocity.normalized;
+		float forward = Vector3.Dot (heading, transform.forward);
+
+		float speed = body.velocity.magnitude;
+		anim.SetFloat ("speed", speed);
+		anim.SetFloat ("forwardVelocity", forward * speed);
+		anim.SetFloat ("turnVelocity", body.angularVelocity.y);
+
+
+
+
+		//Stupid Anim Stuff
 		if (Input.GetButtonDown ("Jump")) {
 			anim.SetTrigger ("playerDeath");
 		}
@@ -50,4 +68,15 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Keypad2))
 			anim.SetTrigger ("backFlip");
 	}
+	void FixedUpdate()
+	{
+		Vector3 v = body.velocity;
+		Vector3 heading = v.normalized;
+		float speed = v.magnitude;
+
+		float turnSpeed = Vector3.Cross (transform.forward, heading).y * maxTurnSpeed;
+		body.angularVelocity = Vector3.up * turnSpeed;
+	}
+
+
 }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class CameraController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public float moveSpeed = 1;
+	public float turnSpeed = 5;
+	public Transform target;
+	public Vector3 offset;
+	public Vector3 lookOffset;
+
+	void Update () 
+	{
+		if (target == null) 
+		{
+			target = GameObject.FindWithTag ("Player").transform;
+		}
+
+		transform.position = Vector3.Lerp 
+		(
+			transform.position,
+				target.position + target.right * offset.x + target.up * offset.y + target.forward * offset.z,
+			Time.deltaTime * moveSpeed
+		);
+
+		transform.rotation = Quaternion.Slerp 
+		(
+			transform.rotation,
+			Quaternion.LookRotation ((target.position + lookOffset) - transform.position),
+			Time.deltaTime * turnSpeed
+		);
 	}
 }
