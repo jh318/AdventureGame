e559f557cee8e454bda5d1a5c48c834f0e203307
John Hawley <jhawley@institute.local>
2017-05-18 21:38:43 -0500

    created enemy controller contains a lot of stuff. cloned gummy man to make test enemy.

--- Assets/Character/Scripts/PlayerController.cs
+++ Assets/Character/Scripts/PlayerController.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float maxSpeed = 5;
+	public float maxSpeedChange = 0.3f;
 	public float maxTurnSpeed = 3.5f;
-	public float maxForwardVelocity = 5f;
-	public float maxTurnVelocity = 3.5f;
+	//public float maxForwardVelocity = 5f;
+	//public float maxTurnVelocity = 3.5f;
 
 	private Animator anim;
 	private HealthController health;
 	private Rigidbody body;
+	private Vector3 targetVelocity;
 
 
 	// Use this for initialization
@@ -35,20 +37,18 @@ public class PlayerController : MonoBehaviour {
 		Transform cam = Camera.main.transform;
 		Vector3 targetRight = Input.GetAxis ("Horizontal") * cam.right;
 		Vector3 targetForward = Input.GetAxis ("Vertical") * cam.forward;
-		Vector3 target = targetRight + targetForward;
-		target.y = 0;
-
-		body.velocity = (target).normalized * maxSpeed;
+		targetVelocity = targetRight + targetForward;
+		targetVelocity.y = 0;
+		targetVelocity = targetVelocity.normalized * maxSpeed;
 
 		Vector3 heading = body.velocity.normalized;
 		float forward = Vector3.Dot (heading, transform.forward);
+
 
 		float speed = body.velocity.magnitude;
 		anim.SetFloat ("speed", speed);
 		anim.SetFloat ("forwardVelocity", forward * speed);
 		anim.SetFloat ("turnVelocity", body.angularVelocity.y);
-
-
 
 
 		//Stupid Anim Stuff
@@ -73,6 +73,11 @@ public class PlayerController : MonoBehaviour {
 		Vector3 v = body.velocity;
 		Vector3 heading = v.normalized;
 		float speed = v.magnitude;
+
+		Vector3 velocityChange = targetVelocity - v;
+		velocityChange = velocityChange.normalized * Mathf.Clamp (velocityChange.magnitude, -maxSpeedChange, maxSpeedChange);
+		velocityChange.y = 0;
+		body.AddForce (velocityChange, ForceMode.VelocityChange);
 
 		float turnSpeed = Vector3.Cross (transform.forward, heading).y * maxTurnSpeed;
 		body.angularVelocity = Vector3.up * turnSpeed;
---
+++ Assets/Enemy/EnemyController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+	public float maxSpeed = 5;
+	public float maxTurnSpeed = 3.5f;
+	public float maxSpeedChange = 0.5f;
+	public float wanderAmount = 1;
+
+	private Animator anim;
+	private Rigidbody body;
+	private Transform target;
+
+	Vector3 targetVelocity;
+	Vector3 targetFacing;
+
+
+	//Vector3 heading = (target.position - transform.position).normalized;
+	//Vector3 targetVelocity = heading * maxSpeed;
+	//		Vector3 heading = body.velocity.normalized;
+
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		body = GetComponent<Rigidbody> ();
+		target = GameObject.FindWithTag ("Player").transform;
+		StartCoroutine ("WanderCoroutine");
+	}
+
+	IEnumerator WanderCoroutine(){
+		bool seesTarget = false;
+		while (!seesTarget) {
+			targetVelocity = transform.forward * maxSpeed + Random.insideUnitSphere * wanderAmount;
+			targetFacing = targetVelocity.normalized;
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+
+	void Update(){
+		Vector3 heading = body.velocity.normalized;
+		float forward = Vector3.Dot (heading, transform.forward);
+
+		float speed = body.velocity.magnitude;
+		anim.SetFloat ("speed", speed);
+		anim.SetFloat ("forwardVelocity", forward * speed);
+		anim.SetFloat ("turnVelocity", body.angularVelocity.y);
+	}
+	
+	void FixedUpdate () {
+		if (target == null) return;
+
+
+
+		Vector3 velocityChange = targetVelocity - body.velocity;
+		velocityChange.y = 0;
+		velocityChange = velocityChange.normalized * Mathf.Clamp (velocityChange.magnitude, 0, maxSpeedChange);
+		body.AddForce (velocityChange, ForceMode.VelocityChange);
+
+		float turnSpeed = Vector3.Cross (transform.forward, targetFacing).y * maxTurnSpeed;
+		body.angularVelocity = Vector3.up * turnSpeed;
+	}
+}
