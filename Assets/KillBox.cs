2a5cd17d46cfbee8aa08f19ebe92e177fa717522
John Hawley <jhawley@institute.local>
2017-06-09 18:35:45 -0500

    added back to doors, finished setting up cameras, added invisible walls to outside

---
+++ Assets/KillBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillBox : MonoBehaviour {
+
+	public string deathMessage = "Game Over";
+	public Text textBox;
+
+	void Start(){
+		textBox = TextManager.instance.textBox;
+	}
+
+	void OnTriggerEnter(Collider c){
+		if (c.tag == "Player") {
+			c.gameObject.SetActive (false);
+			textBox.text = deathMessage;
+			textBox.gameObject.SetActive (true);
+		}
+	}
+}
